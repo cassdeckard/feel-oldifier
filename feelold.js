4909ad80c9ff6829cea234484d8bc32f85def9c4
Matt Deckard <histumness@gmail.com>
2016-05-23 22:54:45 -0500

    Take in user age and calculate year of mid-adolescence

--- feelold.js
+++ feelold.js
@@ -1,10 +1,47 @@
+NormalAdolescentYearStatement = 'That means you were in your mid-teens around {{ userAdolescentYear }}.';
+YoungsterAdolescentYearStatement = 'Look, no offense, but you\'re pretty young. This app is designed to make old people feel old and you just simply aren\'t old enough. So get off my lawn.';
+
 angular.module('feel-oldifier',[]). 
 	config(function($routeProvider) {
 		$routeProvider.
-			when('/', { controller: FeelOldifier, templateUrl: 'include.html' });
-	});
+			when('/', {
+                controller: FeelOldifier,
+                templateUrl: 'include.html'
+            });
+	}).
+    directive('adolescentYearStatement', AdolescentYearStatement);
 
 	
-function FeelOldifier($scope) {
-	// TODO
+function FeelOldifier($scope, $compile) {
+    function calculateAdolescentYearFromAge(age) {
+        var currentYear = new Date().getFullYear();
+        return (currentYear - age + 15);
+    }
+
+    $scope.$watch('userAge', function(newVal, oldVal) {
+        $scope.userAdolescentYear = calculateAdolescentYearFromAge(newVal);
+    });
+
+    $scope.userAgeValid = function() {
+        return /^\+?\d+$/.test($scope.userAge);
+    };
+}
+
+
+function AdolescentYearStatement() {
+    return {
+        restrict: 'E',
+        template: '<div ng-show="showNormalStatement()">' +
+            NormalAdolescentYearStatement +
+            '</div>' +
+            '<div ng-hide="showNormalStatement()">' +
+            YoungsterAdolescentYearStatement +
+            '</div>' +
+            '',
+        link: function(scope, elem) {
+            scope.showNormalStatement = function() {
+                return (scope.userAge > 25);
+            }
+        }
+    };
 }
